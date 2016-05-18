a6a4fc6894fac0fc8ef5a2257e79686fd1088b66
SideCI <support@sideci.com>
2016-05-18 06:20:37 +0000

    Applied fixes from SideCI

--- app/helpers/lazy_render/lazyload_helper.rb
+++ app/helpers/lazy_render/lazyload_helper.rb
@@ -1,13 +1,12 @@
 module LazyRender
   module LazyloadHelper
     def lazy_render(name, locals: {}, cache: false, callback: false, parallel: false)
-      data = {'lazy-render-name' => name}
+      data = { 'lazy-render-name' => name }
       data['lazy-render-params'] = locals if locals.instance_of?(Hash) && locals.presence
       data['lazy-render-cache'] = cache.to_i if cache
       data['lazy-render-callback'] = callback if callback
       data['lazy-render-parallel'] = parallel if parallel
       content_tag :span, nil, class: "js-lazy-render-#{name}", data: data
     end
-
   end
 end
